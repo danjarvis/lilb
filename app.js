8d7d3be2d60122fd8adeba2efd75f0a44287f44b
Dan Jarvis <djarvis@kmbs.konicaminolta.us>
2012-06-05 15:30:50 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express')
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('*', routes.index);
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
